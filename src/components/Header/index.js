6ac14b3faa06833122530630d8b7c4366a8982b4
AlexSt <dazerotes@gmail.com>
2023-04-26 13:27:16 +0300

    Edited header, created base for Router

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,10 +7,9 @@ const Header = () => {
         <div className='header-container'>
             <h1 className="heading">AnimalRecogniser</h1>
             <ul className='navbar-container'>
-                <li>Home</li>
-                <li>About us</li>
                 <li>Our pets</li>
                 <li>Blog</li>
+                <li>About us</li>
             </ul>     
         </div>
     );
