27023b5358b25fee3cc577009bc35fa1e1651c9f
AlexSt <dazerotes@gmail.com>
2023-05-11 17:40:01 +0300

    Added SignUp page

---
+++ src/Pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import './style.scss';
+
+const SignUp = () => {
+    return (
+        <>
+            <div className='signup-container'></div>
+        </>
+    );
+    }
+
+export default SignUp;
