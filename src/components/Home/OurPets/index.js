252e00fd8b7a74ce7be8b3e2e26b0108cfc4f8f7
Kotusyk <melnikanna524@gmail.com>
2023-05-19 19:46:57 +0300

    Added fetching of data from backend

--- src/components/Home/OurPets/index.js
+++ src/components/Home/OurPets/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './style.scss';
+import Pet from './Pet';
 
 const OurPets = () => {
+
+    const [dogs, setDogs] = useState([]);
+    const [cats, setCats] = useState();
+
+    useEffect( () =>{
+        const  getPet = async () => {
+        try {
+          const responseFromGetDog = await axios.get("https://localhost:7216/api/Pets/Dog")
+          const responseFromGetCat = await axios.get("https://localhost:7216/api/Pets/Cat")
+
+          await setDogs(responseFromGetDog.data);
+          await setCats(responseFromGetCat.data)
+
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      getPet();
+ 
+    }, [])
+
     return (
         <div className="pets-container">
             <div className="our-pets-header">
@@ -12,14 +35,63 @@ const OurPets = () => {
                 <p>Best pets are waiting for adoption! <br/>Find the one you will love the most with our tools.</p>
             </div>
 
+            
             <div className="pets-block">
+
                 <div className='cats-block'>
                     <div className="pets-block-title">
                         <img src="./images/Home/paw.png"/>
                         <h1>Cats</h1>
                     </div>
                     <div className="cards-container">
-                        <div className='card'>
+                    {
+                        cats !== undefined ? 
+                            ( cats.map((cat) => (       
+                            <Pet key={cat.id} pet={cat}/>
+                            )))
+                        : 'No cats'                         
+                    }
+                    </div>
+                </div>  
+
+                <div className='dogs-block'>
+               
+                    <div className="pets-block-title">
+                        <img src="./images/Home/paw.png"/>
+                        <h1>Dogs</h1>
+                    </div>
+                    <div className="cards-container">
+                    {
+                        dogs !== undefined ? 
+                            ( dogs.map((dog) => (       
+                                <Pet key={dog.id} pet={dog}/>
+                            )))
+                        : 'No dogs'
+                    } 
+                    </div>
+                    
+                </div>    
+            </div>
+
+        </div>
+    );
+}
+
+export default OurPets;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {/* <div className='card'>
                             <img src="./images/cats/cat_1.jpg"/>
                             <h4 className='pet-name'>Lary</h4>
                             <p className='shelter-name'>Shelter “Happy Paw”,</p>
@@ -43,23 +115,12 @@ const OurPets = () => {
                             <p className='shelter-name'>Shelter “Whiskas”,</p>
                             <p className='location'>Ternopil</p>
                         </div>
-                        
-                    </div>
-                </div>  
+                         */}
 
-                <div className='dogs-block'>
-                    <div className="pets-block-title">
-                        <img src="./images/Home/paw.png"/>
-                        <h1>Dogs</h1>
-                    </div>
-                    <div className="cards-container">
-                        <div className='card'>
-                            <img src="./images/dogs/dog_1.jpg"/>
-                            <h4 className='pet-name'>Jack</h4>
-                            <p className='shelter-name'>Shelter “Happy Paw”,</p>
-                            <p className='location'>Ternopil</p>
-                        </div>
-                        <div className='card'>
+
+
+                         
+                        {/* <div className='card'>
                             <img src="./images/dogs/dog_2.jpg"/>
                             <h4 className='pet-name'>Rex</h4>
                             <p className='shelter-name'>Shelter “Hot-Dog”,</p>
@@ -76,14 +137,6 @@ const OurPets = () => {
                             <h4 className='pet-name'>Ben</h4>
                             <p className='shelter-name'>Shelter “Hot-Dog”,</p>
                             <p className='location'>Ternopil</p>
-                        </div>
+                        </div> */}
                         
-                    </div>
-                </div>    
-            </div>
-
-        </div>
-    );
-}
-
-export default OurPets;
+                
